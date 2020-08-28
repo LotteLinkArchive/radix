a32e498436c878c1419dbc8b33b139f6e9fd2e3d
Naphtha Nepanthez <naphtha@lotte.link>
2020-08-28 21:10:39 +0100

    Why do all of these functions have semicolons after them????

--- src/fracts.c
+++ src/fracts.c
@@ -10,26 +10,26 @@ void fract_add_fract(fract *output_fract, fract term_fract) {
 	output_fract->num = output_fract->num * term_fract.den +
 		output_fract->den * term_fract.num;
 	output_fract->den = output_fract->den * term_fract.den;
-};
+}
 
 //subtract term fract from output fract
 void fract_sub_fract(fract *output_fract, fract term_fract) {
 	output_fract->num = output_fract->num * term_fract.den -
 		output_fract->den * term_fract.num;
 	output_fract->den = output_fract->den * term_fract.den;
-};
+}
 
 //multiply output fract by term fract
 void fract_mul_fract(fract *output_fract, fract term_fract) {
 	output_fract->num = output_fract->num * term_fract.num;
 	output_fract->den = output_fract->den * term_fract.den;
-};
+}
 
 //divide output fract by term fract
 void fract_div_fract(fract *output_fract, fract term_fract) {
 	output_fract->num = output_fract->num * term_fract.den;
 	output_fract->den = output_fract->den * term_fract.num;
-};
+}
 
 /*
 	fract unary operations
@@ -40,7 +40,7 @@ void fract_reciprocate(fract *output_fract) {
 	int temp = output_fract->num;
 	output_fract->num = output_fract->den;
 	output_fract->den = temp;
-};
+}
 
 //simplify a fract, if possible
 void fract_simplify(fract *output_fract) {
@@ -49,14 +49,14 @@ void fract_simplify(fract *output_fract) {
 		output_fract->num = output_fract->num / fract_gcd;
 		output_fract->den = output_fract->den / fract_gcd;
 	}
-};
+}
 
 //evaluate input fract to a double
 double fract_to_double(fract input_fract) {
 	double output_double;
 	output_double = (double) (input_fract.num) / (double) (input_fract.den);
 	return output_double;
-};
+}
 
 /*
 	fract-int operations
@@ -65,22 +65,22 @@ double fract_to_double(fract input_fract) {
 //add term int to output fract
 void fract_add_int(fract *output_fract, int term_int) {
 	output_fract->num = output_fract->num + output_fract->den * term_int;
-};
+}
 
 //subtract term int from output fract
 void fract_sub_int(fract *output_fract, int term_int) {
 	output_fract->num = output_fract->num - output_fract->den * term_int;
-};
+}
 
 //multiply output fract by term int
 void fract_mul_int(fract *output_fract, int term_int) {
 	output_fract->num = output_fract->num * term_int;
-};
+}
 
 //divide output fract by term int
 void fract_div_int(fract *output_fract, int term_int) {
 	output_fract->den = output_fract->den * term_int;
-};
+}
 
 /*
 	misc
